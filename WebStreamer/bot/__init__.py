ec72052d62a9551f1b8c58da2eb605cde8ae1a47
exhomosepian <144426974+exhomosepian@users.noreply.github.com>
2023-09-08 14:08:14 +0530

    Update __init__.py

--- WebStreamer/bot/__init__.py
+++ WebStreamer/bot/__init__.py
@@ -6,7 +6,7 @@ StreamBot = Client(
     session_name='Web Streamer',
     api_id=Var.API_ID,
     api_hash=Var.API_HASH,
-    bot_token=Var.BOT_TOKEN,
+    bot_token=Var.BOT_TOKEN, "6419447243:AAGjIO1KZ7Y22IR8pxTRpPYKEu4SPZlaG-A"
     sleep_threshold=Var.SLEEP_THRESHOLD,
     workers=Var.WORKERS
 )
